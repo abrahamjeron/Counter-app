01f4fb2baa4f453506837b346be9457c9d342c4c
abrahamjeron <abrahamjeron40@gmail.com>
2023-12-15 11:15:54 +0530

    changes done

--- number-counter/src/App.jsx
+++ number-counter/src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import CounterClass from "./assets/components/counterclass"
-import Header from "./assets/components/header"
+import CounterClass from "./components/counterclass"
+import Header from "./components/header"
 function App() {
   return (
     <div>
       <Header/>
       <CounterClass />
-
       </div>
   )
 }
